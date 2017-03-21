aa5405088d55960b40003e072be8f7e2052ad74a
Helen Chen <medea99@gmail.com>
2017-03-21 15:53:23 -0700

    Add wine to dropdown

--- load_data_config.js
+++ load_data_config.js
@@ -31,6 +31,39 @@ var data_config = [
         animate: {
             isAnimate: true
         }
+    },
+    {
+        name: "wine",
+        path: "../data/wine-jittered-all.csv",
+        attr: {
+            all: [
+                '',
+                'Class',                          // 1
+                'Alcohol',                        // 2
+                'Malic acid',                     // 3
+                'Ash',                            // 4
+                'Alcalinity of ash',              // 5
+                'Magnesium',                      // 6
+                'Total phenols',                  // 7
+                'Flavanoids',                     // 8
+                'Nonflavanoid phenols',           // 9
+                'Proanthocyanins',                // 10
+                'Color intensity',                // 11
+                'Hue',                            // 12
+                'OD280/OD315 of diluted wines',   // 13
+                'Proline'                         // 14
+            ],
+	        use: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+	    },
+        category: {
+            name: "Class",
+            index: 1,
+            values: ['1', '2', '3']
+        },
+        animate: {
+	        isAnimate: true,
+            initDraw: true
+	    }
     }
 ];
 
